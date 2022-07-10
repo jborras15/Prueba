ddc21c5a1061ea73c0ed781814e8b7deb8c9a241
jborras15 <jborras15@gmail.com>
2022-07-10 13:07:10 -0500

    se agrego la clase modelo de user y rol.
    Ademas se crea una relacion de la tabla user y rol en una tabla llamada users_roles, por medio de sus id

---
+++ src/main/java/com/example/pruebatecnica/entity/Role.java
@@ -0,0 +1,20 @@
+package com.example.pruebatecnica.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="role")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private  String name;
+
+
+
+
+}
